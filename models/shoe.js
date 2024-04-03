703b32f108b04e0272d95b29bba0f183ba96e2cb
bst003 <bstanner33@yahoo.com>
2024-04-02 23:35:14 -0400

    Create style and shoe models

---
+++ models/shoe.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const shoeSchema = new Schema({
+  name: { type: String, required: true, maxlength: 150 },
+  description: { type: String, maxlength: 300 },
+  price: { type: Number, required: true },
+  brand: { type: Schema.Types.ObjectId, ref: Brand, required: true },
+  style: { type: Schema.Types.ObjectId, ref: Style },
+});
+
+shoeSchema.virtual("url").get(function () {
+  return `/shoe/${this._id}`;
+});
+
+const Shoe = mongoose.model("shoe", shoeSchema);
+
+module.exports = Shoe;
---
+++ models/style.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const styleSchema = new Schema({
+  name: { type: String, required: true, maxlength: 150 },
+});
+
+styleSchema.virtual("url").get(function () {
+  return `/style/${this._id}`;
+});
+
+const Style = mongoose.model("Style", styleSchema);
+
+module.exports = Style;
