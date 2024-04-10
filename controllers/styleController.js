3401e259371a03503baaaeb0f88711594ab9834c
bst003 <bstanner33@yahoo.com>
2024-04-09 23:07:51 -0400

    Set up styles detail view and controller function

--- controllers/styleController.js
+++ controllers/styleController.js
@@ -1,6 +1,7 @@
 const async_handler = require("express-async-handler");
 
 const Style = require("../models/style");
+const Shoe = require("../models/shoe");
 
 const styleController = {};
 
@@ -16,7 +17,18 @@ styleController.styles_list = async_handler(async (req, res, next) => {
 });
 
 styleController.styles_detail = async_handler(async (req, res, next) => {
-  res.send("NOT YET IMPLEMENTED, STYLES DETAIL");
+  const [style, shoesInStyle] = await Promise.all([
+    Style.findById(req.params.id).exec(),
+    Shoe.find({ style: req.params.id }).exec(),
+  ]);
+
+  console.log(style);
+
+  res.render("styles_detail", {
+    title: "Style: ",
+    style,
+    shoesInStyle,
+  });
 });
 
 styleController.styles_create_get = async_handler(async (req, res, next) => {
