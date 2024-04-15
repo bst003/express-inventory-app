9754dccf9d19ab46d902171832f9cb0b5a20142e
bst003 <bstanner33@yahoo.com>
2024-04-14 20:57:39 -0400

    Set up styles create controllers and view

--- controllers/styleController.js
+++ controllers/styleController.js
@@ -1,4 +1,5 @@
 const async_handler = require("express-async-handler");
+const { query, validationResult, body } = require("express-validator");
 
 const Style = require("../models/style");
 const Shoe = require("../models/shoe");
@@ -32,12 +33,53 @@ styleController.styles_detail = async_handler(async (req, res, next) => {
 });
 
 styleController.styles_create_get = async_handler(async (req, res, next) => {
-  res.send("NOT YET IMPLEMENTED, STYLES CREATE GET");
+  const postUrl = req.originalUrl;
+
+  res.render("styles_form", {
+    title: "Create Style",
+    postUrl,
+  });
 });
 
-styleController.styles_create_post = async_handler(async (req, res, next) => {
-  res.send("NOT YET IMPLEMENTED, STYLES CREATE POST");
-});
+styleController.styles_create_post = [
+  body("name")
+    .isLength({ min: 3, max: 150 })
+    .trim()
+    .escape()
+    .withMessage("Name must be between 3 and 150 characters"),
+  async_handler(async (req, res, next) => {
+    const result = validationResult(req);
+
+    console.log(req);
+
+    // if errors return shoes_form and list errors
+    if (!result.isEmpty()) {
+      console.log(result);
+
+      const postUrl = req.originalUrl;
+
+      res.render("shoes_form", {
+        title: "Create Shoe",
+        postUrl,
+        errors: result.errors,
+      });
+    }
+
+    // If no errors then create shoe and redirect to shoe detail page
+    const styleDetails = {
+      name: req.body.name,
+    };
+
+    const newStyle = new Style(styleDetails);
+    await newStyle.save();
+
+    res.render("styles_detail", {
+      title: "Styles: ",
+      shoesInStyle: [],
+      style: styleDetails,
+    });
+  }),
+];
 
 styleController.styles_delete_get = async_handler(async (req, res, next) => {
   res.send("NOT YET IMPLEMENTED, STYLES DELETE GET");
--- models/style.js
+++ models/style.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const styleSchema = new Schema({
-  name: { type: String, required: true, maxlength: 150 },
+  name: { type: String, required: true, minlength: 3, maxlength: 150 },
 });
 
 styleSchema.virtual("url").get(function () {
